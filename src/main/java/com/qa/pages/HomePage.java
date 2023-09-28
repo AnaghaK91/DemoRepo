1178b054f62f10a4e06583f7eb27078118a0fc72
Anagha <k.anagha1991@gmail.com>
2023-09-28 21:39:44 +0530

    new line added

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -5,6 +5,7 @@ public class HomePage {
 	public void HomePgae() {
 		System.out.println("This is my home page");
 		System.out.println("This is another test for git branch");
+		System.out.println("Branch Test");
 	}
 
 }
